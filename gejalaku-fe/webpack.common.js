20d4ba3fb76958b9f427d1830102b62a824b75bd
DzakyFawwaz <dzkyfwzaryd@gmail.com>
2025-05-15 10:15:09 +0700

    fix build image

--- gejalaku-fe/webpack.common.js
+++ gejalaku-fe/webpack.common.js
@@ -9,6 +9,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/gejalaku-app/', // Set the public path for GitHub Pages
   },
   module: {
     rules: [
