bd132755b9f842ff0085654ed2a3522267a67a46
DzakyFawwaz <dzkyfwzaryd@gmail.com>
2025-05-26 09:01:29 +0700

    fix: update BASE_URL to remove trailing slash in production environment

--- gejalaku-fe/src/scripts/config.js
+++ gejalaku-fe/src/scripts/config.js
@@ -1,5 +1,5 @@
 export const ACCESS_TOKEN_KEY = 'accessToken';
 export const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://gejalaku-be.vercel.app/'
+    ? 'https://gejalaku-be.vercel.app'
     : 'http://localhost:8888';
