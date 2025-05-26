56a522597c1ec2a2bcc88e7de4ef3351bfa3e4af
DzakyFawwaz <dzkyfwzaryd@gmail.com>
2025-05-26 08:41:17 +0700

    Refactor HTML structure in index.html for improved readability; update .gitignore to exclude Vercel deployment files; add Vercel configuration for backend deployment; modify BASE_URL in config.js to support production and development environments.

--- gejalaku-fe/src/scripts/config.js
+++ gejalaku-fe/src/scripts/config.js
@@ -1,2 +1,5 @@
 export const ACCESS_TOKEN_KEY = 'accessToken';
-export const BASE_URL = 'http://localhost:8888';
+export const BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://gejalaku-be.vercel.app/'
+    : 'http://localhost:8888';
