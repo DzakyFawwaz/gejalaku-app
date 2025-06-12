3ef1e136901e4ac659299a58bef81e0410bed44a
Dzaky Fawwaz <dzaky@Dzaky.local>
2025-06-13 00:42:27 +0700

    update

--- gejalaku-fe/src/scripts/pages/auth/login/login-page.js
+++ gejalaku-fe/src/scripts/pages/auth/login/login-page.js
@@ -11,8 +11,9 @@ export default class LoginPage {
               <label
                 for="email"
                 class="block mb-1 text-gray-900 font-semibold text-sm"
-                >Email</label
               >
+                Email
+              </label>
               <input
                 id="email"
                 name="email"
@@ -24,8 +25,9 @@ export default class LoginPage {
               <label
                 for="password"
                 class="block mb-1 text-gray-900 font-semibold text-sm"
-                >Password</label
-                >
+              >
+                Password
+              </label>
               <input
                 id="password"
                 name="password"
@@ -34,25 +36,15 @@ export default class LoginPage {
                 class="w-full mb-2 px-3 py-2 border border-gray-300 rounded-md text-gray-500 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                 required
               />
-              <div class="flex items-center mb-6">
-              <input
-                  id="remember"
-                  name="remember"
-                  type="checkbox"
-                  class="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
-              />
-              <label for="remember" class="ml-2 block text-gray-700 text-sm"
-                  >Remember me</label
-              >
-              </div>
               <button id="login-button type="submit" class="w-full cursor-pointer bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 rounded-md mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-1">Log In</button>
               <p class="text-center text-gray-700 text-sm">
-              Don't have an account?
-              <a
-                href="#/register"
-                class="cursor-pointer text-blue-600 font-semibold hover:underline"
-                >Register</a
-              >
+                Don't have an account?
+                <a
+                  href="#/register"
+                  class="cursor-pointer text-blue-600 font-semibold hover:underline"
+                >
+                  Register
+                </a>
               </p>
           </form>
     `;
