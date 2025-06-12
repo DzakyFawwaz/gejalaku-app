043bb6154f1dfc16e2e7c44e090cd9e33e5cb0bc
Dzaky Fawwaz <dzaky@Dzaky.local>
2025-06-12 21:45:11 +0700

    heroku

--- gejalaku-fe/src/scripts/config.js
+++ gejalaku-fe/src/scripts/config.js
@@ -1,5 +1,5 @@
 export const ACCESS_TOKEN_KEY = 'accessToken';
 export const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://gejalaku-be.vercel.app'
+    ? 'https://warm-falls-63385-d8f38de035b0.herokuapp.com'
     : 'http://localhost:8888';
